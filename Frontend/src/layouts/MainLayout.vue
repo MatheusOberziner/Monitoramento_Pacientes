9e96766edb4ff8002ef840c961690d9988efe996
Matheus Schmidt Oberziner <matheus.oberziner@unidavi.edu.br>
2023-09-29 08:26:05 -0300

    Construido layout toolbar e página home

--- Frontend/src/layouts/MainLayout.vue
+++ Frontend/src/layouts/MainLayout.vue
@@ -1,116 +1,71 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
+      <q-toolbar class="justify-between" style="background-color: #f7b7b7;">
+          <div class="row q-px-md q-gutter-md">
+            <div>
+              <img src="../assets/logo.png" style="height: 80px; width: 80px;" />
+            </div>
+            <div class="self-center">
+              <span class="text-h4 text-bold" style="color: black;">Monitoramento de Pacientes</span>
+            </div>
+          </div>
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
+          <div class="row q-px-md q-gutter-md">
+            <div>
+              <q-btn round>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="1.5em"
+                  viewBox="0 0 24 24"
+                  color="black"
+                ><path
+                  fill="currentColor"
+                  d="M18 10a6 6 0 0 0-12 0v8h12v-8Zm2 8.667l.4.533a.5.5 0 0 1-.4.8H4a.5.5 0 0 1-.4-.8l.4-.533V10a8 8 0 1 1 16 0v8.667ZM9.5 21h5a2.5 2.5 0 0 1-5 0Z"
+                /></svg>
+                <q-badge
+                  floating
+                  color="red"
+                  rounded
+                />
+              </q-btn>
+            </div>
 
-        <div>Quasar v{{ $q.version }}</div>
+            <div>
+              <q-btn round>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="1.5em"
+                  viewBox="0 0 24 24"
+                  color="black"
+                ><path
+                  fill="currentColor"
+                  d="M12 2C6.486 2 2 6.486 2 12v4.143C2 17.167 2.897 18 4 18h1a1 1 0 0 0 1-1v-5.143a1 1 0 0 0-1-1h-.908C4.648 6.987 7.978 4 12 4s7.352 2.987 7.908 6.857H19a1 1 0 0 0-1 1V18c0 1.103-.897 2-2 2h-2v-1h-4v3h6c2.206 0 4-1.794 4-4c1.103 0 2-.833 2-1.857V12c0-5.514-4.486-10-10-10z"
+                /></svg>
+              </q-btn>
+            </div>
+
+            <div>
+              <q-btn-dropdown round icon="person" style="color: black;" />
+            </div>
+          </div>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
-
     <q-page-container>
-      <router-view />
+      <q-page padding>
+        <router-view />
+      </q-page>
     </q-page-container>
   </q-layout>
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
 
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
-
-export default defineComponent({
+export default {
   name: 'MainLayout',
 
-  components: {
-    EssentialLink
-  },
-
   setup () {
-    const leftDrawerOpen = ref(false)
-
-    return {
-      essentialLinks: linksList,
-      leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
-    }
   }
-})
+}
 </script>
--- Frontend/src/pages/IndexPage.vue
+++ Frontend/src/pages/IndexPage.vue
@@ -1,17 +1,155 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+  <div class="row">
+    <div class="col-12">
+      <div class="col-12 col-md-12">
+        <q-card>
+          <q-card-section class="q-pb-none">
+            <div class="row">
+              <div class="col-12 col-md-4">
+                <h4 class="q-px-md">
+                  Lista de pacientes
+                </h4>
+              </div>
+
+              <div class="col-12 col-md-8 q-pr-md row self-center justify-end">
+                <div class="col-12 col-md-5 q-pr-md">
+                  <q-input
+                    v-model="search"
+                    outlined
+                    :label="'Pesquisar por:'"
+                    class=""
+                    @keyup.enter="returnPatients()"
+                  >
+                    <template #append>
+                      <q-btn-dropdown
+                        flat
+                        split
+                        color="black"
+                        icon="search"
+                        @click="returnPatients()"
+                      >
+                        <q-list
+                          dense
+                          bordered
+                          separator
+                          style="width: 12em;"
+                        >
+                          <q-item
+                            v-for="(filtro, index) in flitros"
+                            :key="index"
+                          >
+                            <q-item-section side>
+                              <q-radio
+                                v-model="filtroSelecionado"
+                                dense
+                                checked-icon="task_alt"
+                                unchecked-icon="panorama_fish_eye"
+                                :label="filtro.label"
+                                :val="filtro.value"
+                              />
+                            </q-item-section>
+                          </q-item>
+                        </q-list>
+                      </q-btn-dropdown>
+                    </template>
+                  </q-input>
+                </div>
+
+                <div class="q-mt-sm">
+                  <q-btn
+                    label="Adicionar novo"
+                    icon="add"
+                    class="q-mb-md"
+                    style="background-color: #f7b7b7;"
+                    @click="showRegister()"
+                  />
+                </div>
+              </div>
+            </div>
+          </q-card-section>
+          <q-card-section>
+            <div class="q-pa-md">
+              <q-table
+                :rows="rows"
+                :columns="columns"
+                :rows-per-page-options="[5,10,15]"
+                :loading="loading"
+              >
+                <template #body-cell-actions="props">
+                  <q-td :props="props" class="q-gutter-sm">
+                    <q-btn
+                      icon="info"
+                      color="blue"
+                      dense
+                      size="sm"
+                      style="width: 45px;"
+                      @click="showUpdate(props.row)"
+                    />
+                  </q-td>
+                </template>
+
+                <template #no-data>
+                  <q-icon
+                    name="warning"
+                    color="warning"
+                    size="xs"
+                    class="q-pa-sm"
+                  />
+                  <span style="font-size: 12px;">Nenhum dado encontrado!</span>
+                </template>
+              </q-table>
+            </div>
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
+  </div>
 </template>
-
 <script>
-import { defineComponent } from 'vue'
+import { ref } from 'vue'
 
-export default defineComponent({
-  name: 'IndexPage'
-})
+export default {
+  name: 'IndexPage',
+  setup () {
+    const loading = ref(false)
+    const filtroSelecionado = ref('nome')
+    const search = ref('')
+
+    return {
+      filtroSelecionado,
+      search,
+      loading
+    }
+  },
+
+  data () {
+    return {
+      filtros: [
+        {
+          label: 'Nome',
+          value: 'nome'
+        },
+        {
+          label: 'Idade',
+          value: 'idade'
+        },
+        {
+          label: 'Cidade',
+          value: 'cidade'
+        }
+      ],
+      columns: [
+        { name: 'nome', align: 'left', label: 'Nome', field: 'nome', sortable: true },
+        { name: 'cpf', align: 'left', label: 'CPF', field: 'cpf', sortable: true },
+        { name: 'sexo', align: 'left', label: 'Sexo', field: 'sexo', sortable: true },
+        { name: 'idade', align: 'left', label: 'Idade', field: 'idade', sortable: true },
+        { name: 'cidade', align: 'left', label: 'Cidade', field: 'cidade', sortable: true },
+        { name: 'actions', align: 'left', label: 'Ações', field: 'actions' }
+      ],
+      rows: [
+        { nome: 'Matheus', cpf: '999.999.999-99', sexo: 'Masculino', idade: '20', cidade: 'Rio do Sul' }
+      ]
+    }
+  }
+}
 </script>
